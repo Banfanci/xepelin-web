2f5fef4a88fc511580c4e7d3c710a1d3ba00d2a1
banfanci <bflobos@uc.cl>
2022-11-14 00:18:20 -0300

    console.log

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log("the AUTH0_SECRET env var is set: ", !!process.env.AUTH0_SECRET);
+  res.status(200).json({ name: "John Doe" });
 }
